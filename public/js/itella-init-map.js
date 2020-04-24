24a01155c1d837bb48d8b0390f41cb935ff03411
Mantas <mantas.tarutis@gmail.com>
2020-04-24 12:16:40 +0300

    rewritten in jquery

--- public/js/itella-init-map.js
+++ public/js/itella-init-map.js
@@ -3,8 +3,8 @@
 
     // init itella map in cart page
     $(document.body).on('updated_shipping_method', () => {
-         if (document.getElementById('shipping_method_0_itella_pp').checked) {
-            if(!document.querySelector('.itella-shipping-container')) {
+         if ($('#shipping_method_0_itella_pp').is(':checked')) {
+            if(!$('.itella-shipping-container')) {
                 init();
             }
          }
@@ -12,8 +12,8 @@
 
     // init itella map in checkout page
     $(document.body).on('updated_checkout', () => {
-        if (document.getElementById('shipping_method_0_itella_pp').checked) {
-            if(!document.querySelector('.itella-shipping-container')) {
+        if ($('#shipping_method_0_itella_pp').is(':checked')) {
+            if(!$('.itella-shipping-container')) {
                 init();
             }
         }
