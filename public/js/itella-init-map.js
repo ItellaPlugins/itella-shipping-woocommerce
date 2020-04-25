83fd4ca9465fd1076cef291f78922025ada8d440
Mantas <mantas.tarutis@gmail.com>
2020-04-25 09:00:45 +0300

    init map fix

--- public/js/itella-init-map.js
+++ public/js/itella-init-map.js
@@ -3,8 +3,8 @@
 
     // init itella map in cart page
     $(document.body).on('updated_shipping_method', () => {
-         if ($('#shipping_method_0_itella_pp').is(':checked')) {
-            if(!$('.itella-shipping-container')) {
+        if ($('#shipping_method_0_itella_pp').is(':checked')) {
+            if(!$('.itella-shipping-container').val()) {
                 init();
             }
          }
@@ -13,7 +13,7 @@
     // init itella map in checkout page
     $(document.body).on('updated_checkout', () => {
         if ($('#shipping_method_0_itella_pp').is(':checked')) {
-            if(!$('.itella-shipping-container')) {
+            if(!$('.itella-shipping-container').val()) {
                 init();
             }
         }
