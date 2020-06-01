228860630f8be3e302fa73804061492760cd6d7f
miketango30 <mantas.tarutis@gmail.com>
2020-06-01 10:33:48 +0300

    map display fixed in cart page

--- public/js/itella-init-map.js
+++ public/js/itella-init-map.js
@@ -3,6 +3,7 @@
 
     // init itella map in cart page
     $(document.body).on('updated_shipping_method', () => {
+        // console.log('updated_shipping_method')
         if ($('#shipping_method_0_itella_pp').is(':checked')) {
             if(!$('.itella-shipping-container').length) {
                 itella_init();
@@ -12,6 +13,16 @@
 
     // init itella map in checkout page
     $(document.body).on('updated_checkout', () => {
+        // console.log('updated_checkout')
+        if ($('#shipping_method_0_itella_pp').is(':checked')) {
+            if(!$('.itella-shipping-container').length) {
+                itella_init();
+            }
+        }
+    });
+
+    $(document.body).on('updated_wc_div', () => {
+        // console.log('updated_wc_div')
         if ($('#shipping_method_0_itella_pp').is(':checked')) {
             if(!$('.itella-shipping-container').length) {
                 itella_init();
