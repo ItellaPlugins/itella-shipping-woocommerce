5103f4129cc31bf0cdc2585753f22831dbd8610a
Marijus Kundelis <markakk@gmail.com>
2020-10-07 15:39:35 +0300

    Improvement: Shipping method params

--- admin/js/itella-shipping-admin.js
+++ admin/js/itella-shipping-admin.js
@@ -8,6 +8,7 @@
         const courier = document.querySelector('.courier-method');
         const pickupPointFields = document.querySelectorAll('.pickup-point');
         const courierFields = document.querySelectorAll('.courier');
+        const allfields = document.querySelectorAll('.itella-settings input');
         // const feeTaxField = document.querySelector('.method-fee-tax').parentElement.parentElement.parentElement.parentElement;
 
         if (!pickupPoint.checked) {
@@ -37,6 +38,21 @@
 			// if (feeTaxField.classList.contains('d-none')) {
 			// 	feeTaxField.classList.toggle('d-none');
 			// }
+        });
+
+        allfields.forEach(field => {
+            field.addEventListener('invalid', function () {
+                this.parentElement.parentElement.parentElement.classList.add("invalid-value");
+                var first_invalid = document.querySelector('.itella-settings .invalid-value input');
+                first_invalid.scrollIntoView({
+                    behavior: 'auto',
+                    block: 'center',
+                    inline: 'center'
+                });
+            });
+            field.addEventListener('change', function () {
+                this.parentElement.parentElement.parentElement.classList.remove("invalid-value");
+            });
         });
     });
 })();
